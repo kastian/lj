6020a528ac1562c78b94938b1738e98ba31ac8e1
kstn <kastian@mail.ru>
2015-06-04 15:08:57 +0300

    lj v0.9.1

--- lj.sh
+++ lj.sh
@@ -4,9 +4,9 @@
 
 # Copyright 2015 #kstn
 
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
@@ -15,39 +15,23 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ### Global variables ###################################################
-VERSION="0.9"
+VERSION="0.9.1"
 CONFIG_DIR="$HOME/.lj"
 CONFIG_FILE="${CONFIG_DIR}/lj"
 COOKIE="/tmp/lj_cookie"
 LJ_FORM="/tmp/lj_form"
-LOG="/tmp/lj_log"
 
-### Bas config file ####################################################
-CONFIG_BAS=$(cat <<EOF
-# -*- mode: shell-script; -*-
-# basic config for lj
-# uncomment to use
-USER_AGENT='Mozilla/5.0 (X11; Linux i686; rv:38.0) Gecko/20100101 Firefox/38.0' # Masking for browser - for i686
-# USER_AGENT='Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0' # Masking for browser - for x86_64
-# LJ_USER="username"    # Default lj to post (only login)
-# LJ_PASS="password"    # Default password. IMPORTANT - this is unsafe option
-# LJ_SECURITY="private" # Default security (public, friends, private)
-# LJ_ADULT="default"    # prop_adult_content (default, none, explicit)
-# end of file
-EOF
-)
-
-die()  { echo "$1" >&2 ; exit 1; }
+die()  { echo "$0: $1" >&2 ; exit 1; }
 ok()   { echo "$(tput hpa 70)[$(tput setf 2)OK$(tput setf 7)]" ; }
-fail() { echo "$(tput hpa 70)[$(tput setf 4)FAIL$(tput setf 7)]"; exit 1; }
+fail() { echo "$(tput hpa 70)[$(tput setf 4)FAIL$(tput setf 7)]"; }
 
-[[ "${EDITOR}" ]] || die "\$EDITOR is not set. Set it or run lj as 'EDITOR=\"emacs\" lj'"
-# ### Parse command line args ############################################
+### Check $EDITOR ######################################################
+[[ "${EDITOR}" ]] || die "\$EDITOR is not set. Set it or run lj as 'EDITOR=\"emacs\" $0'"
+
+### Parse command line args ############################################
 if [[ "$1" ]]; then
     case "$1" in
 	-c  | --config)
@@ -79,29 +63,48 @@ if [[ "$1" ]]; then
 	    ;;
     esac
 fi
+
 ### Check needed utilites ##############################################
 [[ $(which curl 2>/dev/null) ]]         || die "Can't find curl. Install it"
 [[ $(which mktemp 2>/dev/null) ]]       || die "Can't find mktemp. Install it"
+[[ $(which date 2>/dev/null) ]]         || die "Can't find date. Install it"
+[[ $(which uname 2>/dev/null) ]]        || die "Can't find uname. Install it"
 [[ $(which xmllint 2>/dev/null) ]]      || die "Can't find xmllint. Install it"
+
 ### Check config dir and make if not ###################################
 if [[ ! -d "$CONFIG_DIR" ]]; then
     mkdir "$CONFIG_DIR"                 || die "Can't make $CONFIG_DIR"
 fi
+
 ### Check config file and make if not ##################################
 if [[ ! -f "$CONFIG_FILE" ]]; then
-    echo "$CONFIG_BAS" > "$CONFIG_FILE" || die "Can't make config file ($CONFIG_FILE)"
-    chmod 600 "$CONFIG_FILE"            || die "Can't chmod config file"
+    cat <<EOT > "$CONFIG_FILE"          || die "Can't make config file ($CONFIG_FILE)"
+# -*- mode: shell-script; -*-
+# basic config for lj
+# uncomment to use
+USER_AGENT='Mozilla/5.0 (X11; $(uname -s) $(uname -m); rv:38.0) Gecko/20100101 Firefox/38.0' # Masking for browser
+# LJ_USER="username"    # Default lj to post (only login)
+# LJ_PASS="password"    # Default password. IMPORTANT - this is unsafe option
+# LJ_SECURITY="private" # Default security (public, friends, private)
+# LJ_ADULT="default"    # prop_adult_content (default, none, explicit)
+# end of file
+EOT
+    chmod 600 "$CONFIG_FILE"            || die "Can't chmod config file ($CONFIG_FILE)"
 fi
+
 ### load config ########################################################
 . "$CONFIG_FILE"
+
 ### make masked pass variable ##########################################
 MASK=$(echo "$LJ_PASS" | tr [:print:] '*')
+
 ### get login ##########################################################
 while (true); do
     read -p "LJ [$LJ_USER]: " VAR
     [[ "$VAR" ]] && LJ_USER="$VAR"
     [[ "$LJ_USER" ]] && break
 done
+
 ### get password #######################################################
 while (true); do
     read -s -p "Password [$MASK]: " VAR
@@ -112,14 +115,17 @@ while (true); do
     echo ""
     [[ "$LJ_PASS" ]] && break
 done
+
 ### get subject ########################################################
 IFS="\n" read -p "Subject: " LJ_SUBJECT
 
-### get post ###########################################################
+### get post and check if it is empty ##################################
 TMP_FILE=$(mktemp)
 $EDITOR $TMP_FILE
 LJ_POST=$(cat $TMP_FILE)
+[[ "$LJ_POST" ]] || die "Cowardly refusing to send an empty post"
 
+### get other things ###################################################
 IFS="\n" read -p "Mood: " LJ_MOOD
 IFS="\n" read -p "Music: " LJ_MUSIC
 IFS="\n" read -p "Location: " LJ_LOCATION
@@ -131,6 +137,7 @@ while (true); do
 	1) LJ_SECURITY="public"  ;;
 	2) LJ_SECURITY="friends" ;;
 	3) LJ_SECURITY="private" ;;
+	*) echo "" ;;
     esac
     [[ "$LJ_SECURITY" ]] && break
 done
@@ -157,12 +164,13 @@ echo "Location: $LJ_LOCATION"
 
 echo ""
 read -n 1 -p "Is all right? [y/n]: " VAR
-echo ""
+clear
 
 ### save post ##########################################################
 if [[ "$VAR" != "n" && "$VAR" != "N" ]] ; then
     LJ_FILE="${CONFIG_DIR}/post_$(date '+%s')"
     cat <<EOF > "$LJ_FILE"
+# -*- mode: shell-script; -*-
 LJ_USER='$LJ_USER'
 LJ_DATE='$LJ_DATE'
 LJ_TIME='$LJ_TIME'
@@ -172,73 +180,98 @@ LJ_TAGS='$LJ_TAGS'
 LJ_POST='$LJ_POST'
 EOF
     echo "Post saved to $LJ_FILE"
+else
+    exit 0
 fi
 
 # get cookie
 echo -n "Get cookie..."
-curl -s -v -A "$USER_AGENT" \
-     -b "$COOKIE" -c "$COOKIE" \
-     -d "ret=1" \
-     -d "user=$LJ_USER" \
-     -d "password=$LJ_PASS" \
-     --data-urlencode "action:login=" \
-     -e "http://www.livejournal.com/" \
-     https://www.livejournal.com/login.bml > /dev/null 2>"$LOG"
+STATUS=$(curl -s -A "$USER_AGENT" \
+	      -b "$COOKIE" -c "$COOKIE" \
+	      -d "ret=1" \
+	      -d "user=$LJ_USER" \
+	      -d "password=$LJ_PASS" \
+	      --data-urlencode "action:login=" \
+	      -e "http://www.livejournal.com/" \
+	      -o /dev/null \
+	      -w "%{http_code}" \
+	      https://www.livejournal.com/login.bml )
 
 # check answer
-(( $(grep -E '^< HTTP' "$LOG" | cut -d ' ' -f 3) == 200)) && ok || fail
+case "$STATUS" in
+    200 ) ok   ;;
+    *   ) fail ;
+	  die "HTTP response status code: $STATUS" ;;
+esac
 
 # get lj_form_auth
 echo -n "Get auth..."
-curl -s -v -A "$USER_AGENT" \
-     -b "$COOKIE" -c "$COOKIE" \
-     -e "http://www.livejournal.com/" \
-     http://www.livejournal.com/update.bml > "$LJ_FORM" 2>"$LOG"
+STATUS=$(curl -s -A "$USER_AGENT" \
+	      -b "$COOKIE" -c "$COOKIE" \
+	      -e "http://www.livejournal.com/" \
+	      -o "$LJ_FORM" \
+	      -w "%{http_code}" \
+	      http://www.livejournal.com/update.bml )
 
 # check answer
-(( $(grep -E '^< HTTP' "$LOG" | cut -d ' ' -f 3) == 200)) && ok || fail
+case "$STATUS" in
+    200 ) ok   ;;
+    *   ) fail ;
+	  die "HTTP response status code: $STATUS" ;;
+esac
 
-LJ_FORM_AUTH=$(xmllint --html --xpath 'string(//input[@name="lj_form_auth"]/@value)' "$LJ_FORM" 2>/dev/null)
+# parse auth
+LJ_FORM_AUTH=$(xmllint --html \
+		       --xpath 'string(//input[@name="lj_form_auth"]/@value)' \
+		       "$LJ_FORM" 2>/dev/null)
 
 # send post
 echo -n "Send post..."
-curl -s -v -A "$USER_AGENT" \
-     -b "$COOKIE" -c "$COOKIE" \
-     -e "http://www.livejournal.com/update.bml" \
-     -d "lj_form_auth=$LJ_FORM_AUTH" \
-     -d "rte_on=0" \
-     -d "date_diff=1" \
-     --data-urlencode "date_format=%M/%D/%Y" \
-     -d "postas=remote" \
-     -d "user=" \
-     -d "password=" \
-     -d "postto=journal" \
-     -d "community=" \
-     -d "altcommunity=" \
-     --data-urlencode "date=$LJ_DATE" \
-     --data-urlencode "time=$LJ_TIME" \
-     -d "custom_time=0" \
-     -d "timezone=300" \
-     -d "prop_picture_keyword=" \
-     --data-urlencode "subject=$LJ_SUBJECT" \
-     --data-urlencode "body=$LJ_POST" \
-     --data-urlencode "prop_taglist=$LJ_TAGS" \
-     -d "prop_current_moodid=0" \
-     --data-urlencode "prop_current_mood=$LJ_MOOD" \
-     --data-urlencode "prop_current_music=$LJ_MUSIC" \
-     --data-urlencode "prop_current_location=$LJ_LOCATION" \
-     -d "prop_adult_content=default" \
-     -d "comment_settings=default" \
-     -d "prop_opt_screening=" \
-     -d "security=$LJ_SECURITY" \
-     --data-urlencode "action:update=1" \
-     -d "nojs=1" \
-     http://www.livejournal.com/update.bml > /dev/null 2>"$LOG"
-ok
+STATUS=$(curl -s -A "$USER_AGENT" \
+	      -b "$COOKIE" -c "$COOKIE" \
+	      -e "http://www.livejournal.com/update.bml" \
+	      -d "lj_form_auth=$LJ_FORM_AUTH" \
+	      -d "rte_on=0" \
+	      -d "date_diff=1" \
+	      --data-urlencode "date_format=%M/%D/%Y" \
+	      -d "postas=remote" \
+	      -d "user=" \
+	      -d "password=" \
+	      -d "postto=journal" \
+	      -d "community=" \
+	      -d "altcommunity=" \
+	      --data-urlencode "date=$LJ_DATE" \
+	      --data-urlencode "time=$LJ_TIME" \
+	      -d "custom_time=0" \
+	      -d "timezone=300" \
+	      -d "prop_picture_keyword=" \
+	      --data-urlencode "subject=$LJ_SUBJECT" \
+	      --data-urlencode "body=$LJ_POST" \
+	      --data-urlencode "prop_taglist=$LJ_TAGS" \
+	      -d "prop_current_moodid=0" \
+	      --data-urlencode "prop_current_mood=$LJ_MOOD" \
+	      --data-urlencode "prop_current_music=$LJ_MUSIC" \
+	      --data-urlencode "prop_current_location=$LJ_LOCATION" \
+	      -d "prop_adult_content=default" \
+	      -d "comment_settings=default" \
+	      -d "prop_opt_screening=" \
+	      -d "security=$LJ_SECURITY" \
+	      --data-urlencode "action:update=1" \
+	      -d "nojs=1" \
+	      -o /dev/null \
+	      -w "%{http_code}" \
+	      http://www.livejournal.com/update.bml )
+
+# check answer
+case "$STATUS" in
+    302 ) ok   ;;
+    *   ) fail ;
+	  die "HTTP response status code: $STATUS" ;;
+esac
 
 # clean up
 echo -n "Clean up..."
-[[ "$COOKIE"  ]] && rm "$COOKIE"
-[[ "$LJ_FORM" ]] && rm "$LJ_FORM"
-[[ "$LOG"     ]] && rm "$LOG"
+[[ "$TMP_FILE" ]] && rm "$TMP_FILE"
+[[ "$COOKIE"   ]] && rm "$COOKIE"
+[[ "$LJ_FORM"  ]] && rm "$LJ_FORM"
 ok
